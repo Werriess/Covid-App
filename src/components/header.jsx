126b80fe6bce7cd2746b7c984fc1b6b287ab2c45
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-07-17 16:07:34 +0200

    Home page creation

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "../css/index.css";
+import Nav from "../components/nav"
+
+function Header() {
+  return <div>
+    <header className="header">
+        <Nav />
+    </header>
+  </div> 
+}
+
+export default Header;
---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+import World from "../components/world";
+import '../css/App.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <World />
+    </div>
+  )
+}
+
+export default App;
