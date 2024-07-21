04fab816f6467660ea41568a9580b3cf79544afc
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-07-21 11:07:36 +0200

    Add routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,28 @@
 import Header from "../components/header";
 import World from "../components/world";
-import '../css/App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Stats from "../pages/stats";
+import "../css/App.css";
 
 function App() {
   return (
     <div>
-      <Header />
-      <World />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Header />
+                <World />
+              </div>
+            }
+          />
+          <Route path="/stats" element={<Stats />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/pages/stats.jsx
@@ -0,0 +1,7 @@
+import Header from "../components/header"
+
+function Stats() {
+    return <Header /> 
+}
+
+export default Header
